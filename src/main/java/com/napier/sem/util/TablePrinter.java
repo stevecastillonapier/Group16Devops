9366cc3b21d8cf31a40561ffafa53980420419d1
Steve Castillo <stevemartincastillo@gmail.com>
2025-10-14 16:16:24 -0600

    update comments in TablePrint class

--- src/main/java/com/napier/sem/util/TablePrinter.java
+++ src/main/java/com/napier/sem/util/TablePrinter.java
@@ -2,12 +2,29 @@ package com.napier.sem.util;
 
 import java.sql.*;
 
+    /**
+     * TablePrinter class responsible for formatting and printing the contents of a
+     * {@link ResultSet} object as a structured, human-readable table to the console.
+     */
     public class TablePrinter {
+
+        /**
+         * Prints the data contained in a {@link ResultSet} to standard output.
+         * The table header uses column names, and the rows display string representations
+         * of the data.
+         * @param rs The ResultSet containing the data to be printed. The cursor must be
+         * positioned before the first row.
+         * @throws SQLException If an error occurs while accessing the ResultSet metadata or data.
+         */
         public static void printResultSet(ResultSet rs) throws SQLException {
+
+            // Retrieve metadata to get column count and names
             ResultSetMetaData md = rs.getMetaData();
             int columns = md.getColumnCount();
 
-            // Print header
+            // ------------------
+            // Print Header
+            // ------------------
             for (int i = 1; i <= columns; i++) {
                 System.out.printf("| %-20s", md.getColumnName(i));
             }
