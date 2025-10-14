a970f25a8119f36763ee86b7ddea2ab26f633d0f
Steve Castillo <stevemartincastillo@gmail.com>
2025-10-14 11:50:51 -0600

    update comments in Country

--- src/main/java/com/napier/sem/model/Country.java
+++ src/main/java/com/napier/sem/model/Country.java
@@ -1,24 +1,62 @@
 package com.napier.sem.model;
 
+/**
+ * Represents a Country entity, serving as a Data Transfer Object (DTO) and Model
+ * to hold information about a country from the database.
+ */
 public class Country {
 
+    //Country Code (Primary Key, usually 3 letters).
     public String Code;
+
+    //Name of the country.
     public String Name;
+
+    //Continent the country belongs to.
     public String Continent;
+
+    //Region within the continent.
     public String Region;
+
+    //Total surface area of the country.
     public double SurfaceArea;
+
+    //Year of independence.
     public Integer IndepYear;
+
+    //Total population of the country.
     public int Population;
+    
+    //Life Expectancy.
     public Double LifeExpectancy;
+
+    //Gross National Product (GNP).
     public Double GNP;
+
+    //Old Gross National Product (GNP)
     public Double GNPOld;
+
+    //Local name of the country.
     public String LocalName;
+
+    //Type of government form
     public String GovernmentForm;
+
+    //Name of the Head of State.
     public String HeadOfState;
+
+    //ID of the capital city
     public Integer Capital;
+
+    //Secondary country code
     public String Code2;
+
+    //The name of the capital city
     public String CapitalName;
 
+    /**
+     * Full constructor for initializing all fields.
+     */
     public Country(String code, String name, String continent, String region, double surfaceArea, Integer indepYear, int population, Double lifeExpectancy, Double GNP, Double GNPOld, String localName, String governmentForm, String headOfState, Integer capital, String code2, String capitalName) {
         Code = code;
         Name = name;
@@ -38,6 +76,9 @@ public class Country {
         CapitalName = capitalName;
     }
 
+    /**
+     * Default no-argument constructor.
+     */
     public Country() {
     }
 
@@ -105,6 +146,10 @@ public class Country {
         return CapitalName;
     }
 
+    /**
+     * Provides a detailed string representation of the Country object for logging and debugging.
+     * @return A formatted string showing all field values.
+     */
     @Override
     public String toString() {
         return "Country{" +
